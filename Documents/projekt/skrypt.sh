7db3c89d61d1f8fd861f6e76d7ead87a69cf0158
kondzi56 <kondzi56@wp.pl>
2023-06-27 13:06:05 +0200

    mod

--- Documents/projekt/skrypt.sh
+++ Documents/projekt/skrypt.sh
@@ -1,39 +1,66 @@
 #!/bin/bash
 
-case "$1" in
-  --date)
-    echo "Dzisiejsza data: $(date)"
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --date | -d)
+        echo $(date)
+      shift
+      break
+      ;;
+    --logs | -l)
+       if [ $# -eq 2 ]
+           then
+           for ((i = 0; i <= $2; i++ ))
+               do
+               mkdir log$i
+               echo "log"$i.txt, $0, $(date) >log$i/ log$i.txt
+           done
+       else
+           for i in {1..100}
+               do
+               mkdir log$i
+               echo "log"$i.txt, $0, $(date) > log$i/log$i.txt
+            done
+       fi
+       shift
+       break
+       ;;
+    --help | -h)
+       echo "###########  HELP  #########"
+       echo "--date | -d - wyswietla date"
+       echo "--logs | -l - tworzy 100 plikow z nazwa pliku, nazwa skryptu, data"
+       echo "     arg[]: x - tworzy x plikow --logs, uzycie --logs x np --logs 30"
+       echo "--help | -h - wyswietla help'a"
+       echo "--innit | -i - kolnuje repozytorium do obecnego folderu"
+       echo "--error | -e - tworzy pliki jak --logs, rowniez posiada 1 argument x - ilosc plikow (domyslnie 100)"
+    shift
     ;;
-  --logs)
-    if [ -z "$2" ]; then
-      # Tworzenie 100 plików logx.txt
-      for ((i=1; i<=100; i++)); do
-        filename="log${i}.txt"
-        echo "Nazwa pliku: $filename" > "$filename"
-        echo "Nazwa skryptu: $0" >> "$filename"
-        echo "Data: $(date)" >> "$filename"
-      done
-      echo "Utworzono 100 plików log"
-    else
-      num_logs=$2
-      # Tworzenie określonej liczby plików logx.txt
-      for ((i=1; i<=num_logs; i++)); do
-        filename="log${i}.txt"
-        echo "Nazwa pliku: $filename" > "$filename"
-        echo "Nazwa skryptu: $0" >> "$filename"
-        echo "Data: $(date)" >> "$filename"
-      done
-      echo "Utworzono $num_logs plików log"
-    fi
+    --innit | -i)
+       git clone https://github.com/kondzi56/repo
+    shift
     ;;
-  --help)
-    echo "Dostępne opcje:"
-    echo "--date         - wyświetla dzisiejszą datę"
-    echo "--logs         - tworzy 100 plików logx.txt"
-    echo "--logs <liczba> - tworzy określoną liczbę plików logx.txt"
-    echo "--help         - wyświetla wszystkie dostępne opcje"
-    ;;
-  *)
-    echo "Nieznana opcja. Użyj --help, aby zobaczyć dostępne opcje."
-    ;;
-esac
+    --error | -e)
+       if [ $# -eq 2 ]
+           then
+           for ((i = 0; i <= $2; i++ ))
+               do
+               mkdir error$i
+               echo "error"$i.txt, $0, $(date) >error$i/error$i.txt
+           done
+       else
+           for i in {1..100}
+               do
+               mkdir error$i
+               echo "error"$i.txt, $0, $(date) > error$i/error$i.txt
+            done
+       fi
+       shift
+       break
+       ;;
+     *)
+         echo "WRONG USAGE"
+         echo "Try" $0 "--help or -h"
+     shift
+     ;;
+  esac
+done
